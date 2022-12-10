b540cef4d7faf7919be6e4a3090f9c8954d8ff52
Nezno6 <76010886+Nezno6@users.noreply.github.com>
2022-12-10 11:38:21 +0100

    Add insert client functionality. (#6)

---
+++ src/main/java/pl/sda/task/hibernate/rentalcars/commands/KomendaDodajKlient.java
@@ -0,0 +1,38 @@
+package pl.sda.task.hibernate.rentalcars.commands;
+
+import pl.sda.task.hibernate.rentalcars.database.DataAccessObject;
+import pl.sda.task.hibernate.rentalcars.model.Klient;
+
+public class KomendaDodajKlient implements Komenda{
+    private final DataAccessObject<Klient> dataAccessObject;
+
+    public KomendaDodajKlient() {
+        this.dataAccessObject = new DataAccessObject<>();
+    }
+
+    @Override
+    public String getKomenda() {
+        return "dodaj klient";
+    }
+
+    @Override
+    public void obsluga() {
+        System.out.println("Podaj imie klienta");
+        String imie = Komenda.scanner.nextLine();
+
+        System.out.println("Podaj nazwisko klienta");
+        String nazwisko = Komenda.scanner.nextLine();
+
+        System.out.println("Podaj nip klienta");
+        String nip = Komenda.scanner.nextLine();
+
+        Klient klient = Klient
+                .builder()
+                .imie(imie)
+                .nazwisko(nazwisko)
+                .nip(nip)
+                .build();
+
+        dataAccessObject.insert(klient);
+    }
+}
