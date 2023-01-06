153369103606d6415a803f48e79a6d009f20b4a1
Nez <nezzengame@gmail.com>
2023-01-06 19:51:34 +0100

    Add delete return functionality.

---
+++ src/main/java/pl/sda/task/hibernate/rentalcars/commands/KomendaUsunZwrot.java
@@ -0,0 +1,31 @@
+package pl.sda.task.hibernate.rentalcars.commands;
+
+import pl.sda.task.hibernate.rentalcars.database.DataAccessObject;
+import pl.sda.task.hibernate.rentalcars.model.Wypozyczenie;
+import pl.sda.task.hibernate.rentalcars.model.Zwrot;
+
+public class KomendaUsunZwrot implements Komenda{
+    private final DataAccessObject<Zwrot> dataAccessObject;
+
+    public KomendaUsunZwrot() {
+        this.dataAccessObject = new DataAccessObject<>();
+    }
+
+    @Override
+    public String getKomenda() {
+        return "usun zwrot";
+    }
+
+    @Override
+    public void obsluga() {
+        System.out.println("Podaj id usuwanego zwrotu: ");
+        String idString = Komenda.scanner.nextLine();
+        Long id = Long.parseLong(idString);
+
+        if (dataAccessObject.delete(Zwrot.class, id)) {
+            System.out.println("Usunieto zwrot");
+        } else {
+            System.err.println("Nie udalo sie usunac zwrotu");
+        }
+    }
+}
