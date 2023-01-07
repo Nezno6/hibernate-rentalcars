194d80c0ce95522e3efc9cf8da519d201d234abe
Nez <nezzengame@gmail.com>
2023-01-07 18:14:11 +0100

    Alphabetic sort command.

--- src/main/java/pl/sda/task/hibernate/rentalcars/Main.java
+++ src/main/java/pl/sda/task/hibernate/rentalcars/Main.java
@@ -2,14 +2,20 @@ package pl.sda.task.hibernate.rentalcars;
 
 import pl.sda.task.hibernate.rentalcars.commands.*;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<Komenda> listaKomend = List.of(new KomendaDodajSamochod(), new KomendaListaSamochod(), new KomendaAktualizujSamochod(), new KomendaUsunSamochod(), new KomendaDodajKlient(), new KomendaListaKlient(), new KomendaAktualizujKlient(), new KomendaUsunKlient(), new KomendaDodajWypozyczenie(), new KomendaListaWypozyczenie(), new KomendaUsunWypozyczenie(), new KomendaAktualizujWypozyczenie(), new KomendaDodajZwrot(), new KomendaListaZwrot(), new KomendaAktualizujZwrot(), new KomendaUsunZwrot());
+
+        List<Komenda> listaKomend = Stream
+                .of(new KomendaDodajSamochod(), new KomendaListaSamochod(), new KomendaAktualizujSamochod(), new KomendaUsunSamochod(), new KomendaDodajKlient(), new KomendaListaKlient(), new KomendaAktualizujKlient(), new KomendaUsunKlient(), new KomendaDodajWypozyczenie(), new KomendaListaWypozyczenie(), new KomendaUsunWypozyczenie(), new KomendaAktualizujWypozyczenie(), new KomendaDodajZwrot(), new KomendaListaZwrot(), new KomendaAktualizujZwrot(), new KomendaUsunZwrot())
+                .sorted(Comparator.comparing(Komenda::getKomenda)).toList();
 
         System.out.println("Lista dostepnych komend:");
         listaKomend.forEach(komenda -> System.out.println(komenda.getKomenda()));
+
         System.out.println("\nPodaj komende");
         String komenda = Komenda.scanner.nextLine();
 
